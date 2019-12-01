768ce9cd8b7b6a5d94b91d6c372a979bf6ff9d4e
qiro <qiro@outlook.jp>
2019-12-01 17:22:03 +0900

    カメラデモ完成

--- VGG16Demo.py
+++ VGG16Demo.py
@@ -1,3 +1,5 @@
+# URL: http://cedro3.com/ai/keras-vgg16/
+
 from keras.applications.vgg16 import VGG16, preprocess_input, decode_predictions
 from keras.preprocessing.image import img_to_array, load_img
 import numpy as np
@@ -8,7 +10,7 @@ model.summary()
 
 # 'data' フォルダから画像データの読み込み
 x = []
-img = img_to_array(load_img('./data/yosaku.png', target_size=(244, 244)))
+img = img_to_array(load_img('./data/yosaku.jpg', target_size=(224, 224)))
 x.append(img)
 ary = np.asarray(x)
 print('ary shape:', ary.shape)
